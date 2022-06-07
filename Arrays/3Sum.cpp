47b599f9129ab970d960c5385019f432f6afb0f4
Nilesh Khimani <63780355+NileshKhimani1@users.noreply.github.com>
2022-06-07 19:33:07 +0530

    3Sum
    https://www.codingninjas.com/codestudio/problems/triplets-with-given-sum_893028?topList=striver-sde-sheet-problems&leftPanelTab=1

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -1,39 +1,34 @@
-vector<vector<int>> findTriplets(vector<int>arr, int n, int k) {
+vector<vector<int>> findTriplets(vector<int>arr, int n, int tar) {
     vector<vector<int>> ans;
     if(n <= 2)
         return ans;
     sort(arr.begin(), arr.end());
-    
     for(int i = 0; i<n-2; i++)
     {
-        if(i == 0 or (i>0 and arr[i] != arr[i-1]))
+        if(i == 0 or (i > 0 and arr[i - 1] != arr[i]))
         {
-            int tar = k - arr[i];            // nums[i] considered
-
-            int l = i + 1, r = n - 1;
-
+            int sum = tar - arr[i];
+            int l = i+1, r = n-1;
             while(l < r)
             {
-                int tempSum = arr[l] + arr[r];      // for target sum
-                if(tar == tempSum) // if equals the target, push it into the                                         answer
+                int t = arr[l] + arr[r];
+                if(t == sum)
                 {
-                    vector<int> t;
-                    t.push_back(arr[i]);
-                    t.push_back(arr[l]);
-                    t.push_back(arr[r]);
-                    ans.push_back(t);
-                    
-           // to avoid the duplicated move l and r
-                    while(l < r and arr[l+1] == arr[l])
+                    vector<int> res(3, 0);
+                    res[0] = arr[i];
+                    res[1] = arr[l];
+                    res[2] = arr[r];
+                    ans.push_back(res);
+                    while(l < r and arr[l] == arr[l+1])
                         l++;
-                    while(l < r and arr[r-1] == arr[r])
+                    while(l < r and arr[r] == arr[r-1])
                         r--;
                 }
-                if(tempSum <= tar)
+                if(t <= sum)
                     l++;
                 else
                     r--;
-            }
+            }  
         }
     }
     return ans;
